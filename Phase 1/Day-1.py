2f146c0fa8f04039192efe0127f0e6dd2085070c
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-02 12:27:53 +0300

    Day 1

---
+++ Phase 1/Day-1.py
@@ -0,0 +1,13 @@
+def Check_Spam(Email_Text):
+    Spam_Words = ["مجاني", "جائزة", "ربح", "اضغط هنا"]
+    
+    for word in Spam_Words:
+        if word in Email_Text:
+            return "spam"
+    
+    return "Inbox"
+
+print (Check_Spam("اضغط هنا"))
+print (Check_Spam("ربح"))
+print (Check_Spam("جائزة"))
+print (Check_Spam("مجاني"))
