e9530cf5408d225fd4315c151500656bf4173f29
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-02 12:44:23 +0300

    Edit File

--- Phase 1/Day-2.py
+++ Phase 1/Day-2.py
@@ -17,3 +17,23 @@ print("Input (1, 1) -> Output:")
 print(And_Gate(1, 1))
 print("Input (1, 0) -> Output:" )
 print(And_Gate(1, 0))
+
+def And_Gate(Input1, Input2):
+    
+    Wight1 = 0.5
+    Wight2 = 0.5
+    bias = -0.7
+    
+    Total_Signal = (Input1 * Wight1) + ( Input2 * Wight2) + bias
+    
+    if Total_Signal >= 0:
+        
+            return 1
+    else:
+            return 0
+        
+
+print("Input (1, 1) -> Output:")
+print(And_Gate(1, 1))
+print("Input (1, 0) -> Output:" )
+print(And_Gate(1, 0))
