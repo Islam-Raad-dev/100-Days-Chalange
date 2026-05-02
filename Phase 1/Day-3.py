beab51620d6505c7cf3c5202a13fd7d7551befca
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-02 13:10:29 +0300

    Add

--- Phase 1/Day-3.py
+++ Phase 1/Day-3.py
@@ -1,19 +1,8 @@
-def Process_Batch(Input_List, Weights, Bais):
+def Mean(Numbers):
     
-    results = []
     
-    for Inputs in Input_List:
-        
-        Total_Signal = sum(i * w for i , w in zip(Inputs, Weights)) + Bais
-        
-        Output = 1 if Total_Signal >= 0 else 0
-        
-        results.append(Output)
-        
-    return results
+    
 
-Data = [[1,1], [1,0], [0,1], [0,0]]
-Current_Weights = [0.5, 0.5]
-Current_Bias = - 0.7
+Numbers = []
 
-print(f"Batch Processing Results: {Process_Batch(Data, Current_Weights, Current_Bias)}")
+print(f"The Mean Is: {Mean(Numbers)}")
