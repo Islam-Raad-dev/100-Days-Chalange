3d6fd39f1b02e3668f83e0dc754dbd423da2dd3a
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-02 13:24:20 +0300

    Day 3

---
+++ Phase 1/Day-4.py
@@ -0,0 +1,21 @@
+import time
+
+def Slow_Processing(Data):
+    
+    Satrt_Time = time.time()
+    
+    Resutls = []
+    
+    for x in Data:
+        
+        time.sleep(0.01)
+        
+        Resutls.append(x * 2)
+        
+    End_Time = time.time()
+    
+    return End_Time - Satrt_Time
+
+Data_List = list(range(100))
+
+print(f"Time taken (Sequential): {Slow_Processing(Data_List):.4f} seconds")
