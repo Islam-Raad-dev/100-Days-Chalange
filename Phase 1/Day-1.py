e287ada9e6dc3a9fcb543cdbaa6c2050b97fb8fc
Islam Raad <islamraad.dev@gmail.com>
2026-05-04 19:00:40 +0300

    Fix formatting in Check_Spam function

--- Phase 1/Day-1.py
+++ Phase 1/Day-1.py
@@ -1,6 +1,6 @@
 def Check_Spam(Email_Text):
     Spam_Words = ["مجاني", "جائزة", "ربح", "اضغط هنا"]
-    
+     
     for word in Spam_Words:
         if word in Email_Text:
             return "spam"
